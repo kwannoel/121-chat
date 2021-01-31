6141ea8127ed525d4265ef766645e0b16093fb55
Noel Kwan <noelkwan1998@gmail.com>
2021-01-31 16:55:54 +0800

    Finish input

--- src/components/services/main_control/input.rs
+++ src/components/services/main_control/input.rs
@@ -1,6 +1,15 @@
-use super::messages::Message;
-use crate::services::main_control::Event;
+use std::io;
+use std::io::Read;
 use std::sync::mpsc::Sender;
 
+use super::messages::Message;
+use super::Event;
+
 pub fn start(event_queue: Sender<Event>) {
+    loop {
+        let mut buffer = vec![];
+        let mut stdin = io::stdin();
+        stdin.read(&mut buffer);
+        event_queue.send(Event::SendMsg(buffer));
+    }
 }
