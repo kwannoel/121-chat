ea41792417ce4b9e0ca834530b239bc0af6c6ad9
Noel Kwan <noelkwan1998@gmail.com>
2021-02-01 21:56:35 +0800

    Formatting

--- src/components/config.rs
+++ src/components/config.rs
@@ -8,22 +8,19 @@ pub enum Config {
 impl Config {
     pub fn new() -> Self {
         let mode_key = "MODE";
-        match env::var_os(mode_key) {
-            Some(s) => {
-                if s == "server" {
-                    return Self::Server;
-                } else if s == "client" {
-                    let ip_addr_key = "IP_ADDR";
-                    let ip_addr = match env::var_os(ip_addr_key) {
-                        Some(ia) => String::from(ia.to_str().unwrap()),
-                        _ => String::from("127.0.0.1"),
-                    };
-                    return Self::Client(format!("{}:8000", ip_addr));
-                } else {
-                    panic!("Invalid mode")
-                }
+        if let Some(s) = env::var_os(mode_key) {
+            if s == "server" {
+                return Self::Server;
             }
-            _ => panic!("Mode not supplied"),
-        };
+            if s == "client" {
+                let ip_addr_key = "IP_ADDR";
+                let ip_addr = match env::var_os(ip_addr_key) {
+                    Some(ia) => String::from(ia.to_str().unwrap()),
+                    _ => String::from("127.0.0.1"),
+                };
+                return Self::Client(format!("{}:8000", ip_addr));
+            }
+        }
+        panic!("Mode not supplied");
     }
 }
